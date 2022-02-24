116a78c9cbc914a60bf00825f46963b038fd6701
Daniel McCoy Stephenson <dmccoystephenson@gmail.com>
2022-02-23 22:03:46 -0700

    Update DansPluginManager.java

--- src/main/java/dansplugins/dpm/DansPluginManager.java
+++ src/main/java/dansplugins/dpm/DansPluginManager.java
@@ -136,8 +136,8 @@ public final class DansPluginManager extends PonderBukkitPlugin {
         createRecord("medievalfactions", "https://github.com/Dans-Plugins/Medieval-Factions/releases/download/v4.6.2/Medieval-Factions-4.6.2.jar");
         createRecord("simpleskills", "https://github.com/Dans-Plugins/SimpleSkills/releases/download/v2.0/SimpleSkills-2.0.jar");
         createRecord("wildpets", "https://github.com/Dans-Plugins/Wild-Pets/releases/download/1.4/WildPets-1.4.jar");
-        createRecord("currencies", "https://github.com/Dans-Plugins/Currencies");
-        createRecord("foodspoilage", "https://github.com/Dans-Plugins/FoodSpoilage");
+        createRecord("currencies", "https://github.com/Dans-Plugins/Currencies/releases/download/v1.2/Currencies-1.2.jar");
+        createRecord("foodspoilage", "https://github.com/Dans-Plugins/FoodSpoilage/releases/download/v2.0/FoodSpoilage-v2.0.jar");
     }
 
     private void createRecord(String name, String link) {
