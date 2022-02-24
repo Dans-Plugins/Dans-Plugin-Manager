c928409e55b31c8f909254f3e2cd9e6422a262d0
dmccoystephenson <dmccoystephenson@gmail.com>
2022-02-24 05:16:41 +0000

    Added a utility class for project record initialization.

--- src/main/java/dansplugins/dpm/DansPluginManager.java
+++ src/main/java/dansplugins/dpm/DansPluginManager.java
@@ -9,6 +9,8 @@ import dansplugins.dpm.commands.HelpCommand;
 import dansplugins.dpm.commands.ListCommand;
 import dansplugins.dpm.factories.ProjectRecordFactory;
 import dansplugins.dpm.services.LocalConfigService;
+import dansplugins.dpm.utils.ProjectRecordInitializer;
+
 import org.jetbrains.annotations.NotNull;
 import preponderous.ponder.minecraft.bukkit.abs.AbstractPluginCommand;
 import preponderous.ponder.minecraft.bukkit.abs.PonderBukkitPlugin;
@@ -42,7 +44,7 @@ public final class DansPluginManager extends PonderBukkitPlugin {
         instance = this;
         initializeConfig();
         initializeCommandService();
-        initializeProjectRecords();
+        ProjectRecordInitializer.getInstance().initializeProjectRecords();
     }
 
     /**
@@ -130,17 +132,5 @@ public final class DansPluginManager extends PonderBukkitPlugin {
                 new ListCommand()
         ));
         commandService.initialize(commands, "That command wasn't found.");
-    }
-
-    private void initializeProjectRecords() {
-        createRecord("medievalfactions", "https://github.com/Dans-Plugins/Medieval-Factions/releases/download/v4.6.2/Medieval-Factions-4.6.2.jar");
-        createRecord("simpleskills", "https://github.com/Dans-Plugins/SimpleSkills/releases/download/v2.0/SimpleSkills-2.0.jar");
-        createRecord("wildpets", "https://github.com/Dans-Plugins/Wild-Pets/releases/download/1.4/WildPets-1.4.jar");
-        createRecord("currencies", "https://github.com/Dans-Plugins/Currencies/releases/download/v1.2/Currencies-1.2.jar");
-        createRecord("foodspoilage", "https://github.com/Dans-Plugins/FoodSpoilage/releases/download/v2.0/FoodSpoilage-v2.0.jar");
-    }
-
-    private void createRecord(String name, String link) {
-        ProjectRecordFactory.getInstance().createProjectRecord(name, link);
     }
 }
